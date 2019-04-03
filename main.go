248f7f5cf2828e38c231c62decd0c2bd427ef70b
i-zemlya <github@i-zemlya.ru>
2019-04-03 20:17:37 +0100

    up 2

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-        "fmt"
+	"fmt"
 )
 
 func main() {
-        fmt.Printf("%s\n", "https://i-zemlya.ru")
+	var g *int32
+	g = 637
+	fmt.Printf("%s\n", "https://i-zemlya.ru")
+	fmt.Printf("%+v\n", &g)
 }
