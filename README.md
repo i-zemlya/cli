c016d1abf280d9400305b7f6737a4a900d85aaf4
i-zemlya <github@i-zemlya.ru>
2019-04-06 06:34:57 +0100

    update project

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # cli
 
 cli - test site project, fcgi-server
+
+##
+
+[![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2Fi-zemlya%2Fcli.svg?type=large)](https://app.fossa.com/projects/git%2Bgithub.com%2Fi-zemlya%2Fcli?ref=badge_large)
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"net/http/fcgi"
@@ -11,6 +12,8 @@ import (
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags)
+
 	//var g *int32
 	//*g = 637
 	fmt.Printf("%s\n", "https://i-zemlya.ru")
@@ -25,7 +28,52 @@ func main() {
 	r := http.NewServeMux()
 	r.HandleFunc("/", index)
 	r.HandleFunc("/favicon.ico", favicon)
-	fcgi.Serve(l, r)
+
+	/*for {
+		b, err := net.Accept()
+		if err != nil {
+			continue
+		}
+
+	}*/
+
+	server := NewServer()
+	log.Println("Listening server...")
+	go server.Listen()
+
+	ss := &http.Server{
+		Addr:    ":12190",
+		Handler: server.Handler(),
+	}
+	ss.ListenAndServe()
+
+	s := new(FCgiServer)
+	fcgi.Serve(l, s)
+	//fcgi.Serve(l, r)
+}
+
+type (
+	FCgiServer struct{}
+)
+
+func (s *FCgiServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	defer func() {
+		if err := recover(); err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(`500 Error`))
+			//log.Println(err.Error())
+		}
+	}()
+
+	fmt.Printf("%s\n", r.URL.String())
+	fmt.Printf("%+v\n\n\n", r)
+	if r.URL.String() == "/favicon.png" {
+		w.Write([]byte(``))
+	} else {
+		//w.Write([]byte(`<title>ok</title><link rel="icon" href="/favicon.png" type="image/x-icon"><p>stok</p>`))
+	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
